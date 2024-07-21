08b1cb0b8b54e497ba489a71c01d3d203f99eb5a
valeriaaragon2023 <valeriaaragon@gmail.com>
2024-07-21 19:42:43 -0300

    validacion solo letras

--- js/formulario.js
+++ js/formulario.js
@@ -39,3 +39,25 @@ function validateEmail(email) {
     var re = /^[^\s@]+@(gmail\.com|hotmail\.com)$/;
     return re.test(email);
 }
+
+
+
+//validar que solo se ingresen letras en campos de nombre,  apellido y descripcion
+function soloLetras(e) {
+    var key = e.keyCode || e.which,
+      tecla = String.fromCharCode(key).toLowerCase(),
+      letras = " áéíóúabcdefghijklmnñopqrstuvwxyz",
+      especiales = [39],
+      tecla_especial = false;
+
+    for (var i in especiales) {
+      if (key == especiales[i]) {
+        tecla_especial = true;
+        break;
+      }
+    }
+
+    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+      return false;
+    }
+  }
